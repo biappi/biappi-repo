f34b4bf2b2b9f6f89aa32a05d3c11095ac37a0d8
antonio.malara <antonio.malara@39aeef46-e555-0410-83a8-7bebf669e888>
2008-09-03 12:24:25 +0000

    Added TwitterSilver

---
+++ trunk/TwitterSilver/TwitterSilver.py
@@ -0,0 +1,24 @@
+import objc
+import os
+
+from Foundation import NSLog
+from Cocoa import NSRunAlertPanel
+
+import twitter
+
+QSActionProvider = objc.lookUpClass("QSActionProvider")
+QSObject = objc.lookUpClass("QSObject")
+
+class SendTweetAction(QSActionProvider):
+	def performActionOnObject_(self, object):
+		try:
+			file = open(os.path.expanduser("~/Library/Preferences/org.nnva.biappi.TwitterSilver"), "r")
+			the_user, the_pass = file.readline().strip().split()
+		except IOError:
+			NSLog (str(e))
+			NSRunAlertPanel("No configuration found", "Sorry, i know this is a bit weird, but for configuration you have to put a file named 'org.nnva.biappi.TwitterSilver' in your Library/Preferences folder that contains your credential separated by a space: e.g. 'username password'. If i'll get the time i'll find a way to do a proper config pane", "Ok", None, None)
+		
+		api = twitter.Api(username=the_user, password=the_pass)
+		api.PostUpdate(object.stringValue())
+
+		return None
