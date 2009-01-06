551e9d4a39ff5de097ae99ed83d07df501957b0e
antonio.malara <antonio.malara@39aeef46-e555-0410-83a8-7bebf669e888>
2009-01-06 19:58:49 +0000

    Initial import of REminescence port for iPhone

---
+++ trunk/Sources/REminescenceAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  REminescenceAppDelegate.h
+//  REminescence
+//
+//  Created by Antonio "Willy" Malara on 06/01/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface REminescenceAppDelegate : NSObject <UIApplicationDelegate>
+{
+    UIWindow * window;
+}
+
+@property (nonatomic, retain) UIWindow * window;
+
+@end
---
+++ trunk/Sources/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  REminescence
+//
+//  Created by Antonio "Willy" Malara on 06/01/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	NSInteger           returnValue;
+    NSAutoreleasePool * pool;
+	
+	pool        = [[NSAutoreleasePool alloc] init];
+    returnValue = UIApplicationMain(argc, argv, nil, @"REminescenceAppDelegate");
+    [pool release];
+	
+    return returnValue;
+}
